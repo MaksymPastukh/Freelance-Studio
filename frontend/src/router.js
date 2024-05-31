1dff4069025939b5cf5634ac0ed27988deaa6c97
MaksymPastukh <saponexamq@gmail.com>
2024-05-31 11:50:33 +0200

    Registration and authorization page developed

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -8,6 +8,8 @@ export class Router {
     // Получаем элементы
     this.titilePageElement = document.getElementById('title')
     this.contentPageElement = document.getElementById('content')
+    this.adminlteStyleElement = document.getElementById('adminlte_style')
+
 
 
     this.initEvent()
@@ -18,6 +20,7 @@ export class Router {
         route: '/',
         title: 'Dashboard',
         filePathTemplate: '/templates/dashboard.html',
+        useLayout: '/templates/layout.html',
         load: () => {
           new Dashboard()
         }
@@ -26,6 +29,7 @@ export class Router {
         route: '/404',
         title: 'No page found',
         filePathTemplate: '/templates/404.html',
+        useLayout: false,
         load: () => {
           new NotPageFound()
         }
@@ -34,17 +38,23 @@ export class Router {
         route: '/login',
         title: 'Login',
         filePathTemplate: '/templates/login.html',
+        useLayout: false,
         load: () => {
+          document.body.classList.add('login-page')
           new Login()
-        }
+        },
+        styles: ['icheck-bootstrap.min.css']
       },
       {
         route: '/sign-up',
         title: 'Sign In',
         filePathTemplate: '/templates/sign-up.html',
+        useLayout: false,
         load: () => {
+          document.body.classList.add('register-page')
           new SignUp()
-        }
+        },
+        styles: ['icheck-bootstrap.min.css']
       },
     ]
   }
@@ -65,6 +75,18 @@ export class Router {
     // Проверяем если newRoute существует то мы делаем одно действие
     // Если в newRoute пусто то мы перенаправляем на страницу 404
     if (newRoute) {
+      // Проверяем если newRoute.styles есть данные, то мы обрабатываем эти данные в цикле и добавляем на страницу
+      if(newRoute.styles && newRoute.styles.length > 0) {
+        newRoute.styles.forEach(style => {
+          const link = document.createElement('link')
+          link.rel = 'stylesheet'
+          link.href = '/css/' + style
+          // Добавляем стили в head
+          document.head.insertBefore(link, this.adminlteStyleElement)
+        })
+      }
+
+
       // Делаем проверку на то что в newRoute есть данные title и выполняем действия
       if (newRoute.title) {
         // Присваиваем название страницы в title. В зависимости от той страницы на которой мы находимся
@@ -72,8 +94,31 @@ export class Router {
       }
       //Делаем проверку на то что в newRoute есть данные filePathTemplate. Подгружаем контент на страницу на которой мы находимся
       if (newRoute.filePathTemplate) {
-        // Присваиваем полученный контент. В зависимости от той страницы на которой мы находимся
-        this.contentPageElement.innerHTML = await fetch(newRoute.filePathTemplate)
+        // Очищаем все классы в body
+        document.body.className = ''
+
+        // Переменная в которой храним значения контейнера
+        let contentBlock = this.contentPageElement
+
+        // Проверяем есть ли у нашей страницы использование layout
+        if (newRoute.useLayout) {
+          // Если же layout существует то мы должны отрендерить layout а уже в layout вставить остальной контент
+          // Отрисовка Layout
+          this.contentPageElement.innerHTML = await fetch(newRoute.useLayout)
+            .then(response => response.text())
+          // Меняем контейнер для получения элементов при наличии layout
+          contentBlock = document.getElementById('content-layout')
+          // Добавляем классы к body для адаптации сайт-бара
+          document.body.classList.add('sidebar-mini')
+          document.body.classList.add('layout-fixed')
+        } else {
+          // Удаляем классы к body если на странице нету layout
+          document.body.classList.remove('sidebar-mini')
+          document.body.classList.remove('layout-fixed')
+        }
+        // Если layout нету то мы вставляем на страницу контент без layout
+        // Присваиваем полученный контент.
+        contentBlock.innerHTML = await fetch(newRoute.filePathTemplate)
           .then(response => response.text())
       }
       //Делаем проверку на то что в newRoute есть функция .load и newRoute.load есть function. Подгружаем функционал на страницу на которой мы находимся
